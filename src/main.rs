62eae99d123f18a36be51d11b7c8e01ebd4e3a70
jsmith.entity <jsmith973@protonmail.com>
2025-06-18 20:09:14 +0800

    read todo file contents

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,15 @@
 use crossterm::event::{self, Event};
 use ratatui::{Frame, text::Text};
+use std::env;
+use std::fs;
 
 fn main() {
+    let contents = list_contents();
+    if let Err(e) = contents {
+        println!("{}", e);
+        return;
+    }
+
     let mut terminal = ratatui::init();
     loop {
         terminal.draw(draw).expect("failed to draw frame");
@@ -10,6 +18,19 @@ fn main() {
         }
     }
     ratatui::restore();
+}
+
+fn list_contents() -> Result<String, String> {
+    let args: Vec<String> = env::args().collect();
+    if args.len() < 2 {
+        return Err("Provide one argument pointing to the file to read.".to_string());
+    }
+
+    let file_path: &str = &args[1];
+    match fs::read_to_string(file_path) {
+        Ok(contents) => Ok(contents),
+        Err(e) => Err(e.to_string()),
+    }
 }
 
 fn draw(frame: &mut Frame) {
