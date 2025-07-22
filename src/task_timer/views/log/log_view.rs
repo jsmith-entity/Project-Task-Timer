60b2a729a94c504439fcde3d17c2e6f51eba32f1
jsmith.entity <jsmith973@protonmail.com>
2025-07-22 22:31:07 +0800

    feat: separate log entries by page
    - can navigate down (j) pages
    - can navigate up (k) pages
    - each page hold 9 entries
    - log will hold max 100 entries

--- src/task_timer/views/log/log_view.rs
+++ src/task_timer/views/log/log_view.rs
@@ -45,6 +45,8 @@ pub struct LogView {
     pub logs: Vec<LogEntry>,
 
     selected_filter: Filter,
+    page: usize,
+    page_size: usize,
 }
 
 impl LogView {
@@ -53,15 +55,9 @@ impl LogView {
             logs: Vec::new(),
 
             selected_filter: Filter::default(),
+            page: 0,
+            page_size: 9,
         };
-    }
-
-    pub fn prev_filter(&mut self) {
-        self.selected_filter = self.selected_filter.prev();
-    }
-
-    pub fn next_filter(&mut self) {
-        self.selected_filter = self.selected_filter.next();
     }
 
     pub fn log(&mut self, message: &str, log_type: LogType) {
@@ -71,7 +67,7 @@ impl LogView {
             message: message.to_string(),
         });
 
-        if self.logs.len() >= 40 {
+        if self.logs.len() >= 100 {
             self.logs.remove(0);
         }
     }
@@ -92,10 +88,72 @@ impl LogView {
         match key_code {
             KeyCode::Char('h') => self.prev_filter(),
             KeyCode::Char('l') => self.next_filter(),
+            KeyCode::Char('j') => self.next_page(),
+            KeyCode::Char('k') => self.page = self.page.saturating_sub(1),
             _ => (),
         }
 
         return Ok((InfoSubType::None, "erm".to_string()));
+    }
+
+    fn prev_filter(&mut self) {
+        self.selected_filter = self.selected_filter.prev();
+    }
+
+    fn next_filter(&mut self) {
+        self.selected_filter = self.selected_filter.next();
+    }
+
+    fn next_page(&mut self) {
+        if (self.page + 1) * self.page_size < self.logs.len() {
+            self.page += 1;
+        }
+    }
+
+    fn render_page_status(&self, area: Rect, buf: &mut Buffer) {
+        let total_pages = self.logs.len() / self.page_size + 1;
+
+        let text = format!("↑↓ Page: ({}/{})", self.page + 1, total_pages);
+        Line::from(text).render(area, buf);
+    }
+
+    fn render_log_page(&self, area: Rect, buf: &mut Buffer) {
+        let total_logs = self.logs.len();
+        let end_idx = total_logs.saturating_sub(self.page * self.page_size);
+        let start_idx = end_idx.saturating_sub(self.page_size);
+
+        let dash_line = "-".repeat(area.width as usize);
+
+        Line::from(dash_line.clone()).render(area, buf);
+
+        const ENTRY_HEIGHT: u16 = 2;
+        let mut total_height = 1;
+        for entry in self.logs[start_idx..end_idx].iter().rev() {
+            if !self.selected_filter.includes(entry.log_type) {
+                continue;
+            }
+
+            let entry_area = Rect {
+                x: area.x,
+                y: area.y + total_height,
+                width: area.width,
+                height: ENTRY_HEIGHT,
+            };
+
+            entry.render(entry_area, buf);
+            total_height += entry_area.height;
+
+            let separator_area = Rect {
+                x: area.x,
+                y: area.y + total_height,
+                width: area.width,
+                height: 1,
+            };
+
+            total_height += separator_area.height;
+
+            Line::from(dash_line.clone()).render(separator_area, buf);
+        }
     }
 }
 
@@ -106,39 +164,11 @@ impl Widget for &LogView {
         let vertical = Layout::vertical([Length(2), Min(0)]);
         let [header_area, body_area] = vertical.areas(area);
 
-        self.selected_filter.render(header_area, buf);
+        let horizontal = Layout::horizontal([Min(0), Length(15)]);
+        let [filter_area, page_area] = horizontal.areas(header_area);
 
-        let dash_line = "-".repeat(area.width as usize);
-
-        Line::from(dash_line.clone()).render(body_area, buf);
-
-        const ENTRY_HEIGHT: u16 = 2;
-        let mut total_height = 1;
-        for entry in self.logs.iter().rev() {
-            if !self.selected_filter.includes(entry.log_type) {
-                continue;
-            }
-
-            let entry_area = Rect {
-                x: body_area.x,
-                y: body_area.y + total_height,
-                width: body_area.width,
-                height: ENTRY_HEIGHT,
-            };
-
-            entry.render(entry_area, buf);
-            total_height += entry_area.height;
-
-            let separator_area = Rect {
-                x: body_area.x,
-                y: body_area.y + total_height,
-                width: body_area.width,
-                height: 1,
-            };
-
-            total_height += separator_area.height;
-
-            Line::from(dash_line.clone()).render(separator_area, buf);
-        }
+        self.selected_filter.render(filter_area, buf);
+        self.render_page_status(page_area, buf);
+        self.render_log_page(body_area, buf);
     }
 }
